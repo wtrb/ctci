5dcad6450be5b667fd609c6eb82c2e6b5cc270ac
Thanh Nguyen <thanh.n@kbtg.tech>
2020-05-18 14:13:57 +0700

    double linked list

---
+++ core/data_structures/linked_lists/double-linked-list.go
@@ -0,0 +1,15 @@
+package linkedlist
+
+type DoubleLinkedList struct {
+	Head *dnode
+}
+
+type dnode struct {
+	data int
+	prev *dnode
+	next *dnode
+}
+
+func (l *DoubleLinkedList) Append(data int) {
+	l.Head = nil
+}
